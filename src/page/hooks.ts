d4c59978c7806fa82409af115ff7add6655e21d5
dwqs <461147874@qq.com>
2017-07-14 15:06:55 +0800

    [misc]hooks

---
+++ src/page/hooks.ts
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+// https://github.com/vuejs/vue-class-component#adding-custom-hooks
+Component.registerHooks([
+    'beforeRouteEnter',
+    'beforeRouteLeave',
+]);
