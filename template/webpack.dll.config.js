bf162c2e9e780a6a603405aa949b06beaf6ad745
dwqs <461147874@qq.com>
2017-08-01 00:19:02 +0800

    update dll config

--- template/webpack.dll.config.js
+++ template/webpack.dll.config.js
@@ -16,7 +16,10 @@ let vendors = [
 
 module.exports = {
     entry: {
-        vendor: vendors.concat('vue', 'vue-class-component', 'vue-router', 'babel-polyfill', 'async-await-error-handling')
+        vendor: vendors.concat(
+            'vue', 'vue-class-component', 'vue-router', 'babel-polyfill', 
+            'async-await-error-handling', 'axios'
+        )
     },
     output: {
         path: path.join(__dirname, './dist'),
