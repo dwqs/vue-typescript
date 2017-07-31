68cb22410875afefb238281a51cecc5563732aac
dwqs <461147874@qq.com>
2017-07-31 23:54:15 +0800

    delete store

--- template/src/store/actions.ts
+++
@@ -1,8 +0,0 @@
-// 全局的 actions，触发的 action 会被多个 module 处理
-import { ActionContext } from 'vuex'; 
-
-export default {
-    actionTest({commit}: ActionContext<any, any>, payload: any) {
-           console.log('111---actions');
-    },
-}
--- template/src/store/mutation-types.ts
+++
@@ -1,2 +0,0 @@
-// 所有的 mutation 常量定义
-export const CHANGE_USER_NAME = 'change_user_name';
