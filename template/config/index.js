476ec846aa0fee9b1dfc622886d49fb579968119
dwqs <461147874@qq.com>
2017-07-31 16:31:51 +0800

    update config

--- template/config/index.js
+++ template/config/index.js
@@ -5,17 +5,17 @@ let path = require('path');
 module.exports =  {
     dev:{
         env: 'development',
-        assetsRoot: path.resolve(__dirname, '../{{path}}'),
+        assetsRoot: path.resolve(__dirname, '../dist'),
         assetsPublicPath: '/',
-        contentBase: path.resolve(__dirname, '../{{path}}'),
+        contentBase: path.resolve(__dirname, '../dist'),
         port: {{port}},
         prefix: '{{prefix}}'
     },
     build:{
         env: 'production',
-        assetsRoot: path.resolve(__dirname, '../{{path}}'),
+        assetsRoot: path.resolve(__dirname, '../dist'),
         assetsPublicPath: '/',
-        contentBase: path.resolve(__dirname, '../{{path}}'),
+        contentBase: path.resolve(__dirname, '../dist'),
         prefix: '{{prefix}}'
     }
 }
