54bbcf2d5c4a3b8b25ba9662c4099d5dd9571d15
dwqs <461147874@qq.com>
2018-03-16 14:44:17 +0800

    更新依赖

--- template/postcss.config.js
+++ template/postcss.config.js
@@ -1,11 +1,13 @@
-'use strict';
-
-//fix: https://github.com/akveo/ng2-admin/issues/604
-//使用 happypack 之后 需单独提供 postcss 配置文件
 module.exports = {
     plugins: [
-        require('autoprefixer')({ browsers: ['last 5 versions','Android >= 4.0', 'iOS >= 7'] }),
-        require('precss')(),
-        require('cssnano')({zindex: false})
+        require('autoprefixer')({ 
+            browsers: [
+                'last 5 versions',
+                'Android >= 4.0', 
+                'iOS >= 8',
+                'safari > 8',
+                'not ie <= 8'
+            ] 
+        })
     ]
-}
+};
